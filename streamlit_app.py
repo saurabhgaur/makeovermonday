b6abefab9389f758b129e6e879bc0a6dd82d6a53
Saurabh Gaur <saurabhgaur@gmail.com>
2023-06-19 14:33:49 +0530

    added slider and improved layout a bit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,58 +1,85 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
-# Load the data from the CSV file
-data = pd.read_csv("cereal_production_data.csv")
+# Set page title
+st.set_page_config(page_title="Cereal Production Analysis")
 
-# Set page title and layout using Tailwind CSS
-st.set_page_config(page_title="Cereal Production Dashboard", layout="wide")
+# Add a title to the sidebar section
+st.sidebar.title("Settings")
 
-# Set the title and description of the dashboard
-st.title("Cereal Production Dashboard")
-st.markdown("This dashboard visualizes cereal production data.")
+# Add a title to the main section
+st.title("Cereal Production Analysis")
 
-# Create a sidebar for selecting the country and chart display option
-selected_country = st.sidebar.selectbox("Select Country", data["Entity"].unique())
+
+@st.cache_data
+def get_data():
+    # Load the data
+    data = pd.read_csv("cereal_production_data.csv")
+
+    # Get the unique country names from the data
+    countries = data["Entity"].unique()
+
+    return data, countries
+
+
+# Call the function to get the data and countries
+data, countries = get_data()
+
+
+# Add a selectbox to choose the country
+selected_country = st.sidebar.selectbox(
+    "Select Country",
+    countries.tolist(),
+    index=int(np.where(countries == "World")[0][0]),
+)
+
+# Add a subtitle for the data section
+# Add a title to the line chart
+st.subheader(f"Cereal Production Metrics for {selected_country}")
+# Filter the data for the selected country
+filtered_data = data[data["Entity"] == selected_country].copy()
+
+# Add a slider for setting the starting year
+start_year = st.sidebar.slider(
+    "Starting Year",
+    min_value=int(filtered_data["Year"].min()),
+    max_value=int(filtered_data["Year"].max()),
+)
+
+# Calculate the relative change based on the starting year
+filtered_data.loc[:, "Area harvested (hectares)"] = (
+    filtered_data["Area harvested (hectares)"]
+    / filtered_data.loc[
+        filtered_data["Year"] == start_year, "Area harvested (hectares)"
+    ].values[0]
+) * 100
+filtered_data.loc[:, "Production (tonnes)"] = (
+    filtered_data["Production (tonnes)"]
+    / filtered_data.loc[
+        filtered_data["Year"] == start_year, "Production (tonnes)"
+    ].values[0]
+) * 100
+filtered_data.loc[:, "Yield (tonnes per hectare)"] = (
+    filtered_data["Yield (tonnes per hectare)"]
+    / filtered_data.loc[
+        filtered_data["Year"] == start_year, "Yield (tonnes per hectare)"
+    ].values[0]
+) * 100
+filtered_data.loc[:, "Population (historical estimates)"] = (
+    filtered_data["Population (historical estimates)"]
+    / filtered_data.loc[
+        filtered_data["Year"] == start_year, "Population (historical estimates)"
+    ].values[0]
+) * 100
+
+
+# Add a button for selecting the chart display option
 chart_display_option = st.sidebar.radio(
     "Chart Display Option", ["All Together", "Split by Metric"]
 )
 
-# Filter the data based on the selected country
-filtered_data = data[data["Entity"] == selected_country]
-
-# Calculate the relative change for each series compared to the first year
-first_year = filtered_data["Year"].min()
-
-filtered_data["Area harvested (hectares)"] = (
-    filtered_data["Area harvested (hectares)"]
-    / filtered_data.loc[
-        filtered_data["Year"] == first_year, "Area harvested (hectares)"
-    ].values[0]
-)
-
-filtered_data["Production (tonnes)"] = (
-    filtered_data["Production (tonnes)"]
-    / filtered_data.loc[
-        filtered_data["Year"] == first_year, "Production (tonnes)"
-    ].values[0]
-)
-
-filtered_data["Yield (tonnes per hectare)"] = (
-    filtered_data["Yield (tonnes per hectare)"]
-    / filtered_data.loc[
-        filtered_data["Year"] == first_year, "Yield (tonnes per hectare)"
-    ].values[0]
-)
-
-filtered_data["Population (historical estimates)"] = (
-    filtered_data["Population (historical estimates)"]
-    / filtered_data.loc[
-        filtered_data["Year"] == first_year, "Population (historical estimates)"
-    ].values[0]
-)
-
-# Line chart(s) based on the chart display option
 if chart_display_option == "All Together":
     # Single chart with all metrics together
     fig, ax = plt.subplots(figsize=(10, 6))
@@ -86,6 +113,9 @@ if chart_display_option == "All Together":
     ax.set_title(f"Data for {selected_country}")
     ax.legend()
 
+    # Set x-axis limits
+    ax.set_xlim(start_year, filtered_data["Year"].max())
+
     # Display the chart using Streamlit's native `pyplot` command
     st.pyplot(fig)
 else:
@@ -99,6 +129,7 @@ else:
     axs[0].set_xlabel("Year")
     axs[0].set_ylabel("Relative Change")
     axs[0].set_title("Area harvested")
+    axs[0].set_xlim(start_year, filtered_data["Year"].max())  # Set x-axis limits
 
     axs[1].plot(
         filtered_data["Year"], filtered_data["Production (tonnes)"], color="orange"
@@ -106,6 +137,7 @@ else:
     axs[1].set_xlabel("Year")
     axs[1].set_ylabel("Relative Change")
     axs[1].set_title("Production")
+    axs[1].set_xlim(start_year, filtered_data["Year"].max())  # Set x-axis limits
 
     axs[2].plot(
         filtered_data["Year"],
@@ -115,6 +147,7 @@ else:
     axs[2].set_xlabel("Year")
     axs[2].set_ylabel("Relative Change")
     axs[2].set_title("Yield")
+    axs[2].set_xlim(start_year, filtered_data["Year"].max())  # Set x-axis limits
 
     axs[3].plot(
         filtered_data["Year"],
@@ -124,6 +157,7 @@ else:
     axs[3].set_xlabel("Year")
     axs[3].set_ylabel("Relative Change")
     axs[3].set_title("Population")
+    axs[3].set_xlim(start_year, filtered_data["Year"].max())  # Set x-axis limits
 
     # Adjust spacing between subplots
     fig.tight_layout()
@@ -131,18 +165,5 @@ else:
     # Display the charts using Streamlit's native `pyplot` command
     st.pyplot(fig)
 
-
-# Show the data table
-st.subheader("Data Table")
-st.dataframe(filtered_data)
-
-# Show the description of the columns
-st.subheader("Column Descriptions")
-st.markdown(
-    """
-- Area harvested (hectares): The relative change in the total area of land used for cereal production compared to the first year present in the data.
-- Production (tonnes): The relative change in the total production of cereals compared to the first year present in the data.
-- Yield (tonnes per hectare): The relative change in the average yield of cereals per hectare compared to the first year present in the data.
-- Population (historical estimates): The relative change in the historical population estimates compared to the first year present in the data.
-"""
-)
+# Display the filtered data table
+st.write(filtered_data)
